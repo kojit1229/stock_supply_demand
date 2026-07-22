2a3852b3b8285ef2e2fd66010d4faef3607eaba4
kojit1229 <koji.t1229@gmail.com>
2026-07-22 21:23:00 +0900

    pwa: SWキャッシュ名v3バンプ(PWA変更に伴う必須バンプ)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'jukyu-v2';
+const CACHE_NAME = 'jukyu-v3';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', function (event) {
