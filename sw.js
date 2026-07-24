09891239d5499caf6ab1cc7a907715edbbeb0298
kojit1229 <koji.t1229@gmail.com>
2026-07-24 21:12:01 +0900

    pwa: SWキャッシュ名v8バンプ

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'jukyu-v7';
+const CACHE_NAME = 'jukyu-v8';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', function (event) {
